e4a29618074423f2c3b639573ad45bcbba9e42ca
Сергей <basov888@ya.ru>
2023-01-23 12:44:41 +0700

    Ex02 New project HomeWork2

---
+++ Ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 13. Напищите программу, котарая выводит третью цифру 
+ заданного числа или сообщает, что такой цифры нет. 
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine("Введите число :");
+int numA = int.Parse(Console.ReadLine());
+int result = 0;
+while (numA > 999)
+{
+    numA = numA / 10;
+}
+if (numA < 100)
+{
+    Console.WriteLine($"В введенном Вами числе {numA} нет третьей цифры!");
+}
+else
+{
+    result = numA % 10;
+    Console.WriteLine($"{result} - третья цифра введеного числа");
+}
